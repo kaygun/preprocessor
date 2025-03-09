1232784618104ba26f2ed2ecd27a9400d726ed74
Atabey <atabey_kaygun@hotmail.com>
2025-03-09 21:01:01 +0300

    New versions.

--- markdown.lisp
+++ markdown.lisp
@@ -1,54 +1,59 @@
-(require :cl-ppcre)
-
-(defun helper (x) (eval (read-from-string x)))
+(defun eval-string (x)
+  "Evaluates a string as Lisp code."
+  (eval (read-from-string x)))
 
 (defun process-inline-code (line out)
+  "Processes and writes inline code fragments in a line."
   (let ((pieces (ppcre:split "``" line)))
-    (loop
-       for x in pieces
-       for y from 0 below (length pieces) do
-	 (format out "~a" (if (evenp y) x (helper x))))
+    (loop for x in pieces
+          for i from 0
+          do (format out "~a" (if (evenp i) x (eval-string x))))
     (format out "~%")))
 
-(defun process-header (line)
+(defun parse-header (line)
+  "Parses a header line to determine visibility settings."
   (cond ((ppcre:scan "hide all" line) (list :flag t :code nil :results nil))
-	((ppcre:scan "hide" line) (list :flag t :code nil :results t))
-	(t (list :flag t :code t :results t))))
+        ((ppcre:scan "hide" line) (list :flag t :code nil :results t))
+        (t (list :flag t :code t :results t))))
 
 (defun process-text (line out register)
+  "Processes a text line, checking if it's a header or inline code."
   (if (ppcre:scan "^```" line)
-      (process-header line)
-      (let ()
-	(process-inline-code line out)
-	register)))
+      (parse-header line)
+      (progn
+        (process-inline-code line out)
+        register)))
 
-(let (code-buffer)
-  (defun process-code (line out register)
-    (if (ppcre:scan "^```" line)
-	(let ((results (helper (format nil "(list ~{~a~%~})" code-buffer))))
-	  (if (getf register :code)
-	      (let () (format out "```lisp~%~{~a~%~}```~%" code-buffer)
-		      (if (getf register :results)
-		          (format out "```lisp~%~{~a~%~}```~%" results)))
-	      (if (getf register :results)
-		  (format out "~{~a~%~}" results)))
-	  (setf code-buffer nil)
-	  (list :flag nil))
-	(let ()
-	  (setf code-buffer
-		(append code-buffer (list line)))
-	  register))))
+(defvar *code-buffer* nil)
 
-(with-open-file (in (elt *posix-argv* 1)
-		    :direction :input)
-  (with-open-file (out (elt *posix-argv* 2)
-		       :direction :output
-		       :if-exists :supersede
-		       :if-does-not-exist :create)
-    (let ((register (list :flag nil)))
-      (do ((line (read-line in nil) (read-line in nil)))
-	  ((null line) nil)
-	(setf register      
-	      (if (getf register :flag)
-		  (process-code line out register)
-		  (process-text line out register)))))))
+(defun process-code (line out register)
+  "Processes code blocks, evaluates them, and writes results if required."
+  (if (ppcre:scan "^```" line)
+      (let ((results (eval-string (format nil "(list ~{~a~%~})" *code-buffer*))))
+        (when (getf register :code)
+          (format out "```lisp~%~{~a~%~}```~%" *code-buffer*))
+        (when (getf register :results)
+          (format out "```lisp~%~{~a~%~}```~%" results)))
+      (setf *code-buffer* nil)
+      (list :flag nil))
+    (push line *code-buffer*)
+    register))
+
+(defun process-file (input-file output-file)
+  "Reads an input file, processes its lines, and writes results to the output file."
+  (with-open-file (in input-file :direction :input)
+    (with-open-file (out output-file :direction :output
+                                      :if-exists :supersede
+                                      :if-does-not-exist :create)
+      (let ((register (list :flag nil)))
+        (do ((line (read-line in nil) (read-line in nil)))
+            ((null line) nil)
+          (setf register
+                (if (getf register :flag)
+                    (process-code line out register)
+                    (process-text line out register))))))))
+
+;; Entry point
+(process-file (elt *posix-argv* 1) (elt *posix-argv* 2))
+
+
